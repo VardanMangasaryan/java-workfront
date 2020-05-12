a0c708c5c48b435b9536dea91a2314b245d037de
Vardan <vardanmangasaryan@workfront.com>
2020-05-13 00:17:57 +0400

    1.11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,17 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
+		int sum = 0, number;
+		double average;
 
-		double number1, number2, number3;
 		Scanner input = new Scanner(System.in);
-
-		System.out.println("Input 1st number:");
-		number1 = input.nextDouble();
-
-		System.out.println("Input 2nd number:");
-		number2 = input.nextDouble();
-
-		System.out.println("Input 3rd number:");
-		number3 = input.nextDouble();
-
-		if (number1 > number2 && number1 > number3) {
-			System.out.println("The greatest:" + number1);
-		} else if (number2 > number3) {
-			System.out.println("The greatest:" + number2);
-		} else {
-			System.out.println("The greatest:" + number3);
+		System.out.println("Input the 5 numbers ");
+		for (int i = 0; i < 5; i++) {
+			number = input.nextInt();
+			sum += number;
 		}
+		average = sum / 5;
 
+		System.out.println("The sum of 5 is: " + sum + "\n" + "The average is : " + average);
 	}
 }
