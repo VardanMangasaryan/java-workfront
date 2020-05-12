7e86d0e8c01e2c07b48c884155ed38e91b95c9e5
Vardan <vardanmangasaryan@workfront.com>
2020-05-13 00:55:12 +0400

    1.12

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,15 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		int sum = 0, number;
-		double average;
 
 		Scanner input = new Scanner(System.in);
-		System.out.println("Input the 5 numbers ");
-		for (int i = 0; i < 5; i++) {
-			number = input.nextInt();
-			sum += number;
-		}
-		average = sum / 5;
 
-		System.out.println("The sum of 5 is: " + sum + "\n" + "The average is : " + average);
+		int number = input.nextInt();
+
+		if (number % 2 == 0) {
+			System.out.println("Even");
+		} else {
+			System.out.println("Odd");
+		}
 	}
 }
