0de21177fb8bcce8115102ea8cb50467e47fd669
Vardan <vardanmangasaryan@workfront.com>
2020-05-12 23:22:55 +0400

    1.10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,25 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		double number1, number2, number3;
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Input number:");
-		double number = input.nextDouble();
+		System.out.println("Input 1st number:");
+		number1 = input.nextDouble();
 
-		if (number > 0) {
-			System.out.println("Number is positive");
-		} else if (number < 0) {
-			System.out.println("Number is negative");
+		System.out.println("Input 2nd number:");
+		number2 = input.nextDouble();
+
+		System.out.println("Input 3rd number:");
+		number3 = input.nextDouble();
+
+		if (number1 > number2 && number1 > number3) {
+			System.out.println("The greatest:" + number1);
+		} else if (number2 > number3) {
+			System.out.println("The greatest:" + number2);
 		} else {
-			System.out.println("Number is 0");
+			System.out.println("The greatest:" + number3);
 		}
+
 	}
 }
