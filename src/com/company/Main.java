b0c8b567fb0b58c5c55147c5fee2212ac498de83
Vardan <vardanmangasaryan@workfront.com>
2020-05-12 22:06:27 +0400

    1.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int number1 = 50;
-		int number2 = 3;
-		int sum = number1 / number2;
+		//a
+		System.out.println(-5 + 8 * 6);
 
-		System.out.println(sum);
+		//b
+		System.out.println((55 + 9) % 9);
+
+		//c
+		System.out.println(20 + -3 * 5 / 8);
+
+		//d
+		System.out.println(5 + 15 / 3 * 2 - 8 % 3);
 	}
 }
