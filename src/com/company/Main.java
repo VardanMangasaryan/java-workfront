1a64d42136bf5a5b62cad3872245e89d5cbc7922
Vardan <vardanmangasaryan@workfront.com>
2020-05-12 22:00:51 +0400

    1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,12 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+	public static void main(String[] args) {
+
+		int number1 = 74;
+		int number2 = 36;
+		int sum = number1 + number2;
+
+		System.out.println(sum);
+	}
 }
