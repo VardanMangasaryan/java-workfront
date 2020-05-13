92d2d5e502f4adddebe2add9aa2e2b7950c8843c
Vardan <vardanmangasaryan@workfront.com>
2020-05-13 17:54:29 +0400

    2.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		Random randomGenerator = new Random();
-		int lowerBound, upperBound, randomNumber1, randomNumber2, product, isOdd;
+		int lowerBound, upperBound, randomNumber1, randomNumber2, sum;
 
 		lowerBound = 10;
 		upperBound = 100;
@@ -15,13 +15,10 @@ public class Main {
 		randomNumber1 = randomGenerator.nextInt(upperBound - lowerBound) + lowerBound;
 		randomNumber2 = randomGenerator.nextInt(upperBound - lowerBound) + lowerBound;
 
-		product = randomNumber1 * randomNumber2;
-		isOdd = product % 2;
+		sum = randomNumber1 + randomNumber2;
 
-		if (isOdd == 0) {
-			System.out.println((long) product);
-		} else {
-			System.out.println((double) product);
-		}
+		long longSum = (long) sum;
+
+		System.out.println("First numberis: " + randomNumber1 + "\nSecond number is " + randomNumber2 + "\nSum of two numbers is " + longSum);
 	}
 }
