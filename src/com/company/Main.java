2184df9e82393dae1e071b1b8df9d858e266c64c
Vardan <vardanmangasaryan@workfront.com>
2020-05-15 18:22:00 +0400

    3.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,8 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-
-		for (int i = 1; i < 21; i++) {
-			if (i % 3 == 0) {
-				continue;
-			} else {
-				System.out.println(i);
-			}
+		for (int i = -20; i < 61; i += 2) {
+			System.out.println(i);
 		}
 	}
 }
