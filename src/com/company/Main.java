7449bd7d3ec198561b7a6d7e2d3ee036c8a1c59a
Vardan <vardanmangasaryan@workfront.com>
2020-05-15 20:20:35 +0400

    3.11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,30 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		int number, bound;
+		boolean isPrime = true;
 		Scanner inputNumber = new Scanner(System.in);
 
 		System.out.println("Input the number (Table to be calculated): ");
-		int num = inputNumber.nextInt();
 
-		for (int i = 1; i < 11; i++) {
-			System.out.println(num + " X " + i + " = " + num * i);
+		number = inputNumber.nextInt();
+		bound = Math.abs(number / 2);
+
+
+		if (number == 0 || Math.abs(number) == 1) {
+			System.out.println(number + " is not a prime number");
+			isPrime = false;
+		} else {
+			for (int i = 2; i <= bound; i++) {
+				if (number % i == 0) {
+					System.out.println(number + " is not a prime number");
+					isPrime = false;
+					break;
+				}
+			}
+		}
+		if (isPrime) {
+			System.out.println(number + " is a prime number");
 		}
 	}
 }
