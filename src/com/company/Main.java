b383dff2608326f65cd825d220f3fc3d27479434
Vardan <vardanmangasaryan@workfront.com>
2020-05-15 19:11:00 +0400

    3.9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,18 @@
 package com.company;
 
-import java.sql.*;
 import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int num, sumOfNumbers = 0, averageOfNumbers;
 
 		Scanner inputNumber = new Scanner(System.in);
 
-		for (int i = 0; i < 10; i++) {
-			num = inputNumber.nextInt();
-			sumOfNumbers += num;
+		System.out.println("Input number of terms ");
+		int num = inputNumber.nextInt();
+
+		for (int i = 1; i < num + 1; i++) {
+			System.out.println("Number is: " + i + " and cube of the " + i + "is: " + (int)Math.pow(i, 3));
 		}
-
-		averageOfNumbers = sumOfNumbers / 10;
-
-		System.out.println("Sum of numbers " + sumOfNumbers);
-		System.out.println("Average of numbers " + averageOfNumbers);
 	}
 }
